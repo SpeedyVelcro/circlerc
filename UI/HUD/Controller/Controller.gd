6b4a25b46178f8db60b31bbe5a3d5d9dd3a00727
SpeedyVelcro <32414336+SpeedyVelcro@users.noreply.github.com>
2026-09-15 02:33:05 +0100

    Migrate controller tweens

--- UI/HUD/Controller/Controller.gd
+++ UI/HUD/Controller/Controller.gd
@@ -14,24 +14,20 @@ func _ready():
 
 @warning_ignore("native_method_override") # TODO: rename
 func show():
-	# Inside tree check necessary to prevent annoying error messages when
-			# RC car is deleted
-	if $Tween.is_inside_tree():
-		$Tween.follow_property(get_node("Visual"), "position",
-				$Visual.get_position(), self, "normal_visual_position", hide_time,
-				Tween.TRANS_QUAD, Tween.EASE_OUT)
-		$Tween.start()
+	var tween := create_tween()
+	tween.set_trans(Tween.TRANS_QUAD)
+	tween.set_ease(Tween.EASE_OUT)
+	tween.tween_property($Visual, "position", normal_visual_position, hide_time)
+	tween.play()
 
 
 @warning_ignore("native_method_override") # TODO: rename
 func hide():
-	# Inside tree check necessary to prevent annoying error messages when
-			# RC car is deleted
-	if $Tween.is_inside_tree():
-		$Tween.follow_property(get_node("Visual"), "position",
-				$Visual.get_position(), self, "hidden_visual_position", hide_time,
-				Tween.TRANS_QUAD, Tween.EASE_OUT)
-		$Tween.start()
+	var tween := create_tween()
+	tween.set_trans(Tween.TRANS_QUAD)
+	tween.set_ease(Tween.EASE_OUT)
+	tween.tween_property($Visual, "position", normal_visual_position, hide_time)
+	tween.play()
 
 # Manipulate gizmos
 func steer_left():
@@ -51,8 +47,3 @@ func throttle_stop():
 
 func throttle_reverse():
 	$Visual/ThrottleSprite.play("reverse")
-
-func _on_Controller_tree_exiting():
-	# Stop tween so we don't get annoying error messages (even though they're
-	# silent and don't do anything)
-	$Tween.stop_all()
