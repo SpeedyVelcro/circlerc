7b52243c2ad03ee31287e127d68ba0afe662783e
SpeedyVelcro <32414336+SpeedyVelcro@users.noreply.github.com>
2026-09-16 05:21:39 +0100

    Fix loops not disappearing

--- Entity/Player/Loop/Loop.gd
+++ Entity/Player/Loop/Loop.gd
@@ -75,6 +75,7 @@ func _on_Player_loop_cancel():
 	tween.set_trans(Tween.TRANS_LINEAR)
 	tween.set_ease(Tween.EASE_IN_OUT)
 	tween.tween_property(self, "alpha", 0.0, 0.1)
+	tween.tween_callback(queue_free)
 	tween.play()
 
 func _on_Timer_timeout():
@@ -83,11 +84,8 @@ func _on_Timer_timeout():
 	tween.set_trans(Tween.TRANS_LINEAR)
 	tween.set_ease(Tween.EASE_IN_OUT)
 	tween.tween_property(self, "alpha", 0.0, 3.0)
+	tween.tween_callback(queue_free)
 	tween.play()
-
-func _on_FadeTween_tween_completed(_object, _key):
-	# Faded out so free
-	queue_free()
 
 func _on_Area2D_area_entered(area):
 	# area is known to be a Capturable by collision mask
